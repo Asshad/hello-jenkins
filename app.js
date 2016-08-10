4bebff7413f796c233dd34067f3591406066c1ee
Asshad <Asshad@Asshad-M.local>
2016-08-10 12:21:50 +0800

    Switch to Whatsup Asshad

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('whatsup world');
+  res.send('whatsup asshad');
 });
  
 app.listen(process.env.PORT || 5000);
